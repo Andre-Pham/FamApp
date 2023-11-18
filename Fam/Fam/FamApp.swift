9bd154ed7e80124873046df16fb2c2949276e9fe
Andre Pham <andrepham33@gmail.com>
2023-11-18 13:53:17 +1100

    Project init

---
+++ Fam/Fam/FamApp.swift
@@ -0,0 +1,17 @@
+//
+//  FamApp.swift
+//  Fam
+//
+//  Created by Andre Pham on 18/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct FamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
