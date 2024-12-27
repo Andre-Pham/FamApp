44b0d2314c5dc84cec37935a18c2304ed7109eb4
Andre Pham <andrepham33@gmail.com>
2024-12-28 01:11:40 +1100

    Added mounting function to CanvasController

--- Fam/Fam/Controller Layer/ViewController.swift
+++ Fam/Fam/Controller Layer/ViewController.swift
@@ -32,24 +32,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Step 1: Add the Child View Controller to the Parent
-        addChild(self.canvasController)
-        
-        // Step 2: Set Up the Child View Controller’s View
-        view.addSubview(self.canvasController.view)
-        self.canvasController.view.translatesAutoresizingMaskIntoConstraints = false
-        
-        // Step 3: Apply Auto Layout Constraints
-        NSLayoutConstraint.activate([
-            self.canvasController.view.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 24),
-            self.canvasController.view.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -24),
-            self.canvasController.view.topAnchor.constraint(equalTo: view.topAnchor, constant: 64),
-            self.canvasController.view.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -48)
-        ])
-        
-        // Step 4: Notify the Child View Controller
-        self.canvasController.didMove(toParent: self)
+        let canvasView = self.canvasController.mount(to: self)
+        self.view.add(canvasView)
+        canvasView.constrainAllSides(padding: 50)
         
         self.canvasController.setCanvasBackgroundColor(to: .lightGray)
         
